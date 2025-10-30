254992bae6cfe94707c1671ae8b48c7cfa18b2e4
Himanshi Khatwani <67739958+himanshi-png@users.noreply.github.com>
2025-10-30 18:09:46 +0530

    Update checkspec.cy.js

--- cypress/e2e/checkspec.cy.js
+++ cypress/e2e/checkspec.cy.js
@@ -21,8 +21,8 @@ describe('template spec', () => {
     cy.get('#navbar-search').type('Employee Check');
     cy.get('#awesomplete_list_1').children().first().click();
     cy.get('[data-label="Add Employee Checkin"]').click();
-    // cy.get('.input-with-feedback.form-control.bold[data-fieldname="employee"]')
-    //   .type(Cypress.env('EMP_ID'));
+    cy.get('.input-with-feedback.form-control.bold[data-fieldname="employee"]')
+      .type(Cypress.env('EMP_ID'));
     // cy.get('select[data-fieldtype="Select"]').select('IN');
     cy.get('select[data-fieldtype="Select"][data-fieldname="log_type"]').select('IN');
     cy.wait(1000);
